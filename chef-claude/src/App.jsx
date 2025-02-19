90c7351fdd127974c102727fc55e91977b2dc1d4
Linden Huhmann <lindenhuhmann@gmail.com>
2025-02-19 13:08:17 -0500

    Initialize project and create header

---
+++ chef-claude/src/App.jsx
@@ -0,0 +1,10 @@
+import Header from './components/Header.jsx'
+import './index.css'
+
+export default function App() {
+  return (
+      <>
+        <Header />
+      </>
+  )
+}
---
+++ chef-claude/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import chefIcon from '../images/chef-claude-icon.png'
+
+export default function Header() {
+    return (
+    <header>
+        <nav>
+            <img src={chefIcon} />
+            <span>Chef Claude</span>
+        </nav>
+    </header>
+    )
+}

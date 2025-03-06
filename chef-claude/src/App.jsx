b4129c56000867b844d352dd2c467ee62a9ea747
Linden Huhmann <lindenhuhmann@gmail.com>
2025-03-06 14:35:11 -0500

    Add Main component

--- chef-claude/src/App.jsx
+++ chef-claude/src/App.jsx
@@ -1,10 +1,12 @@
 import Header from './components/Header.jsx'
+import Main from './components/Main.jsx'
 import './index.css'
 
 export default function App() {
   return (
       <>
         <Header />
+        <Main />
       </>
   )
 }
